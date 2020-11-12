367a5b80705337b7d6ee016cf022b4ffddec7d2a
landofcoder <landofcoder@gmail.com>
2020-11-12 11:19:33 +0700

    added module files

---
+++ README.md
@@ -0,0 +1,5 @@
+# Magento 2 Module Lofmp Cashfree Cfcheckout payment compatible with Lof_MarketPlace
+payment method [Cashfree](https://cashfree.com/)
+
+# Payment docs
+[Cashfree Docs](https://docs.cashfree.com/docs/)
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Lof Cashfree payments module for Magento 2
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE,'Lofmp_Cfcheckout',__DIR__);
